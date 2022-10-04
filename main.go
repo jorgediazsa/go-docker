8022210ff3f0ce2042e21984d25d630842c554ec
Jorge Diaz <jorgediazsamaniego@gmail.com>
2022-10-03 22:22:44 -0300

    initial commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"go-docker/database"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.GET("/animal/:name", func(c *gin.Context) {
+		animal, err := database.GetAnimal(c.Param("name"))
+		if err != nil {
+			c.String(404, err.Error())
+			return
+		}
+		c.JSON(200, animal)
+	})
+	r.Run(":4001")
+}
